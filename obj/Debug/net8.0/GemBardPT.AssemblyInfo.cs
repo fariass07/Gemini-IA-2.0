42a19f1a5e1642e08cb283ee6c0d8f7b16142c9a
Bernardo Farias <56223027+fariass07@users.noreply.github.com>
2024-12-07 16:46:12 -0300

    Gemini button added

--- obj/Debug/net8.0/GemBardPT.AssemblyInfo.cs
+++ obj/Debug/net8.0/GemBardPT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GemBardPT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbc2e1e968ca9b223c5736e04014b10353a9740")]
 [assembly: System.Reflection.AssemblyProductAttribute("GemBardPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GemBardPT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
